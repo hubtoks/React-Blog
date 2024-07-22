42ed287f0f9395f6d299fe994047b8cc085351a9
codetoks <1964553188@qq.com>
2024-07-22 23:03:57 +0800

    实现用户信息获取渲染，退出登录清除信息

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -8,6 +8,10 @@ import {
 import './index.scss'
 import { Outlet } from 'react-router-dom'
 import { useNavigate, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchUserInfo,clearUserInfo } from '@/store/modules/userStore'
+
 
 const { Header, Sider } = Layout
 
@@ -31,17 +35,29 @@ const items = [
 
 const ToksLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const location = useLocation();//获取当前URL路径
   const SelectedPath = location.pathname;
+
+  useEffect(() => { 
+    dispatch(fetchUserInfo()) 
+  }, [dispatch])
+
+  const userInfo = useSelector((state) => state.user.userInfo)
+
+  const onConfirm = () => {
+     dispatch(clearUserInfo())
+     navigate('/login') 
+    }
 
   return (
     <Layout>
       <Header className="header" title='后台管理系统'>
-      <div className="logo" />
+        <div className="logo" />
         <div className="user-info">
-          <span className="user-name">柴柴老师</span>
+          <span className="user-name">{userInfo.name}</span>
           <span className="user-logout">
-            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={onConfirm}> 
               <LogoutOutlined /> 退出
             </Popconfirm>
           </span>
@@ -53,14 +69,14 @@ const ToksLayout = () => {
             mode="inline"
             theme="dark"
             defaultSelectedKeys={SelectedPath}   //控制高亮
-            SelectedKeys={SelectedPath}    //控制高亮
+            selectedKeys={SelectedPath}    //控制高亮
             items={items}
             style={{ height: '100%', borderRight: 0 }}
             onClick={({ key }) => navigate(key)}
-            ></Menu>
+          ></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          <Outlet/>
+          <Outlet />
         </Layout>
       </Layout>
     </Layout>
--- src/store/modules/userStore.js
+++ src/store/modules/userStore.js
@@ -1,24 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { request } from "@/utils";
-import { setToken as _setToken, getToken } from "@/utils";
+import { setToken as _setToken, getToken,removeToken} from "@/utils";
 
 const userStore = createSlice({  
     name: "user",
     initialState: {
-        token:getToken('token') || ''  //从本地存储中获取token,  
+        token:getToken('token') || '' , //从本地存储中获取token,  
+        userInfo: {},
     },
     reducers: {
       setToken(state,action){
         state.token = action.payload
         _setToken(action.payload)
-      }
+      },
+      setUserInfo(state, action) { 
+        state.userInfo = action.payload 
+      },
+      clearUserInfo(state) {
+        state.userInfo = {}
+        state.token = ''
+        removeToken() 
+      },
     },
 })
-const {setToken} = userStore.actions
+const {setToken, setUserInfo,clearUserInfo} = userStore.actions
 
 //异步请求
 
-
+//获取用户token
 const fetchLogin = (loginForm) => {
     return async (dispatch) => {
       //请求Token
@@ -27,7 +36,15 @@ const fetchLogin = (loginForm) => {
         dispatch(setToken(res.data.token));  
     }
 }
+//获取用户信息
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+      const res = await request.get("/user/profile");
+      dispatch(setUserInfo(res.data));  
+  }
+}
 
-export { fetchLogin ,setToken };  //按需导出
+
+export { fetchLogin ,fetchUserInfo,setToken,clearUserInfo };  //按需导出
 const reducer = userStore.reducer;
 export default reducer
